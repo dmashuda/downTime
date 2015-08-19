b59af48a08a0e394093a1436248ae1358e729c11
Dan Mashuda <dmashuda@ycp.edu>
2015-08-19 17:03:13 -0400

    added db logging

--- detector.go
+++ detector.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 	"net"
 	"time"
 )
 
 type NetworkResponse struct {
+	gorm.Model
+	Host     string
 	Value    string
 	HasError bool
-	Error    error
+	Error    error `sql:"-"`
 	Time     time.Duration
 }
 
@@ -20,6 +24,7 @@ var host_list = []string{
 	"cloudflare.com:80"}
 
 func main() {
+	dbSetup()
 	channel := make(chan NetworkResponse)
 	go responseHandler(channel)
 
@@ -34,25 +39,48 @@ func main() {
 }
 
 func dialTester(host string, c chan NetworkResponse) {
-
 	for {
 		startTime := time.Now()
 		conn, err := net.DialTimeout("tcp", host, time.Second*20)
 		if err != nil {
-			c <- NetworkResponse{HasError: true, Error: err, Time: time.Since(startTime)}
+			c <- NetworkResponse{HasError: true, Error: err, Time: time.Since(startTime), Host: host}
 		} else {
-			c <- NetworkResponse{HasError: false, Time: time.Since(startTime)}
+			c <- NetworkResponse{HasError: false, Time: time.Since(startTime), Host: host}
 			conn.Close()
 		}
 		time.Sleep(time.Second * 10)
 	}
 }
 
-func responseHandler(c chan NetworkResponse) {
+func dbSetup() {
+	db, err := gorm.Open("sqlite3", "./foo.db")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	db.CreateTable(&NetworkResponse{})
+}
+
+func responseHandler(c chan NetworkResponse) error {
+	db, err := gorm.Open("sqlite3", "./foo.db")
+	defer db.Close()
+
+	if err != nil {
+		return error(err)
+	}
+
+	db.Exec("")
+
 	for {
 		resp := <-c
+		fmt.Println(resp)
 		if resp.HasError {
+			db.Create(&resp)
 			fmt.Println(resp)
 		}
 	}
+
+	return nil
 }
