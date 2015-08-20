7020193496e7bfb1af6acfa0d447406d5a987d2f
Dan Mashuda <dmashuda@ycp.edu>
2015-08-20 14:29:42 -0400

    updated code

--- detector.go
+++ detector.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 	"net"
+	"net/http"
 	"time"
 )
 
 type NetworkResponse struct {
 	gorm.Model
-	Host     string
-	Value    string
-	HasError bool
-	Error    error `sql:"-"`
-	Time     time.Duration
+	Host      string
+	IsGateway bool
+	Value     string
+	HasError  bool
+	Error     error         `sql:"-"`
+	Time      time.Duration `sql:"-"`
 }
 
 var router_ip = "192.168.0.1:80"
@@ -23,29 +26,49 @@ var host_list = []string{
 	"facebook.com:80",
 	"cloudflare.com:80"}
 
+var dbInstance gorm.DB
+
 func main() {
 	dbSetup()
 	channel := make(chan NetworkResponse)
-	go responseHandler(channel)
+	go netTestHandler(channel)
 
 	for z := 0; z < len(host_list); z++ {
-		go dialTester(host_list[z], channel)
+		go dialTester(host_list[z], false, channel)
 	}
-	go dialTester(router_ip, channel)
+	go dialTester(router_ip, true, channel)
+
+	http.HandleFunc("/", httpHandler)
+	http.ListenAndServe(":8080", nil)
 
 	var i int
 	_, err := fmt.Scanf("%d", &i)
 	println(err)
+
 }
 
-func dialTester(host string, c chan NetworkResponse) {
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	db := dbInstance
+
+	results := []NetworkResponse{}
+	db.Where(&NetworkResponse{}).Find(&results)
+
+	jsonVal, jsonerror := json.Marshal(results)
+	if jsonerror != nil {
+		return
+	}
+	fmt.Fprintf(w, string(jsonVal))
+}
+
+func dialTester(host string, isGateway bool, c chan NetworkResponse) {
 	for {
 		startTime := time.Now()
-		conn, err := net.DialTimeout("tcp", host, time.Second*20)
+		conn, err := net.DialTimeout("tcp", host, time.Second*5)
 		if err != nil {
-			c <- NetworkResponse{HasError: true, Error: err, Time: time.Since(startTime), Host: host}
+			val := fmt.Sprintf("%s", err)
+			c <- NetworkResponse{HasError: true, Error: err, Time: time.Since(startTime), Host: host, IsGateway: isGateway, Value: val}
 		} else {
-			c <- NetworkResponse{HasError: false, Time: time.Since(startTime), Host: host}
+			c <- NetworkResponse{HasError: false, Time: time.Since(startTime), Host: host, IsGateway: isGateway}
 			conn.Close()
 		}
 		time.Sleep(time.Second * 10)
@@ -53,32 +76,23 @@ func dialTester(host string, c chan NetworkResponse) {
 }
 
 func dbSetup() {
-	db, err := gorm.Open("sqlite3", "./foo.db")
-	defer db.Close()
-	if err != nil {
+	db, err := gorm.Open("sqlite3", "./failures.db")
+	if err == nil {
 		fmt.Println(err)
-		return
 	}
+	dbInstance = db
 
 	db.CreateTable(&NetworkResponse{})
 }
 
-func responseHandler(c chan NetworkResponse) error {
-	db, err := gorm.Open("sqlite3", "./foo.db")
-	defer db.Close()
-
-	if err != nil {
-		return error(err)
-	}
-
-	db.Exec("")
+func netTestHandler(c chan NetworkResponse) error {
+	db := dbInstance
 
 	for {
 		resp := <-c
-		fmt.Println(resp)
 		if resp.HasError {
-			db.Create(&resp)
 			fmt.Println(resp)
+			db.Create(&resp)
 		}
 	}
 
